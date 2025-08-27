460e9b2c20024f95e94603a1d0c10ca80c4acdf6
Aryan Mishra <mishraaryan9204@gmail.com>
2025-08-27 11:24:35 +0530

    For loop and range function + basic input validation

--- basics/main.go
+++ basics/main.go
@@ -1,11 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 	var conferenceName = "Go Conference"
 	const conferenceTicket = 50
 	var remainingTickets uint = 50
+	bookings := []string{} //slice
 
 	fmt.Printf("conferenceTickets is %T, remainingTickets is %T, conferenceName is %T\n", conferenceTicket, remainingTickets, conferenceName)
 
@@ -19,19 +23,52 @@ func main() {
 		Type inference: But, Go can infer the type when you assign a value
 	*/
 
-	var userName string
-	var email string
-	var userTickets uint
-	//ask user for name:
-	fmt.Printf("Enter your first name: ")
-	fmt.Scan(&userName)
-	fmt.Printf("Enter your email: ")
-	fmt.Scan(&email)
-	fmt.Printf("Enter numberof tickets: ")
-	fmt.Scan(&userTickets)
-	fmt.Printf("User %v with email %v booked %v tickets\n", userName, email, userTickets)
+	for {
+		var userName string
+		var email string
+		var userTickets uint
+		//ask user for name:
+		fmt.Printf("Enter your first name: ")
+		fmt.Scan(&userName)
+		fmt.Printf("Enter your email: ")
+		fmt.Scan(&email)
+		fmt.Printf("Enter number of tickets: ")
+		fmt.Scan(&userTickets)
 
-	remainingTickets = remainingTickets - userTickets
-	fmt.Printf("Thankyou %v for booking %v tickets. You wil get confirmation mail at %v\n", userName, userTickets, email)
-	fmt.Printf("Remaining tickets: %v", remainingTickets)
+		isValidName := len(userName) >= 2
+		isValidEmail := strings.Contains(email, "@")
+		isValidTicketNumber := userTickets > 0 && userTickets <= remainingTickets
+
+		if isValidName && isValidEmail && isValidTicketNumber {
+			fmt.Printf("User %v with email %v booked %v tickets\n", userName, email, userTickets)
+			bookings = append(bookings, userName)
+			remainingTickets = remainingTickets - userTickets
+			fmt.Printf("Thankyou %v for booking %v tickets. You wil get confirmation mail at %v\n", userName, userTickets, email)
+			fmt.Printf("Remaining tickets: %v\n", remainingTickets)
+
+			firstNames := []string{}
+			for _, b := range bookings {
+				names := strings.Fields(b) // -> Split the string with space as seperator, and returns silce with the split elements, Ex: "Aryan Mishra" -> ["Aryan", "Mishra"]
+				firstName := names[0]
+				firstNames = append(firstNames, firstName)
+			}
+			fmt.Printf("This is all the firstNames of bookins are: %v\n", firstNames)
+			if remainingTickets <= 0 {
+				//end the program
+				fmt.Println("Our confernece is booked out. Come back next year.")
+				continue
+			}
+		}
+		if !isValidEmail {
+			fmt.Println("Please enter valid email")
+		}
+		if !isValidName {
+			fmt.Println("Please enter valid name")
+		}
+		if !isValidTicketNumber {
+			fmt.Printf("We only have %v tickets remaining, so you can not book %v tickets\n", remainingTickets, userTickets)
+			fmt.Println("Please try again")
+		}
+
+	}
 }
